9a1098a1a2bf484a5822c47061481a5b14197af6
SP-Upreti <surendraupreti441@gmail.com>
2024-07-11 17:18:05 +0545

    updated subcategory page

--- e-commerce-site/app/[category]/[subcategory]/page.js
+++ e-commerce-site/app/[category]/[subcategory]/page.js
@@ -1,4 +1,5 @@
 "use client"
+import FetchProducts from '@/app/components/FetchProducts';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useParams } from 'next/navigation';
@@ -28,57 +29,65 @@ export default function SubcategoryPage() {
     console.log(data)
 
     return (
-        <section className='pt-32 px-16 flex gap-6 items-center justify-center'>
-            <div className="flex gap-5">
-                <div className="w-1/2 border border-slate-300 h-[20rem] w-[30rem] flex justify-center items-center">
-                    <img src={data.image} alt="" className='w-[12rem]' />
-                </div>
+        <section className='pt-32 px-16 '>
+            <div className="">
+                <div className="flex gap-5 border-b border-slate-400 pb-4">
+                    <div className="w-1/2 border border-slate-300 h-[20rem] w-[30rem] flex justify-center items-center">
+                        <img src={data.image} alt="" className='w-[12rem]' />
+                    </div>
 
-                <div className="w-[40rem] flex flex-col gap-4">
-                    <h2 className='font-bold text-xl'>{data.title}</h2>
-                    <p className='font-semibold text-lg'>Rs.{data.price}</p>
-                    <div className="flex flex-col gap-3">
-                        <p><FontAwesomeIcon icon={faStar} className='text-yellow-500' />
-                            <span> {data.rating ? data.rating.rate : "99"} average ratings <span className='font-semibold'> {`(${data.rating ? data.rating.count : "99"}) reviews`}</span></span>
-                        </p>
-                        <p className="text-lg ">
-                            {data.description}
-                        </p>
-                        <div className="flex gap-4">
-                            <span>Size:</span>
-                            <p className='flex gap-3 items-center '>
-                                {
-                                    [...Array(3)].map(
-                                        (data, index) => {
-                                            return (
-                                                <button className='border border-black px-2 hover:bg-slate-300'>
-                                                    {index + 12}
-                                                </button>
-                                            )
-                                        }
-                                    )
-                                }
+                    <div className=" flex flex-col gap-4">
+                        <h2 className='font-bold text-xl'>{data.title}</h2>
+                        <p className='font-semibold text-lg'>Rs.{data.price}</p>
+                        <div className="flex flex-col gap-3">
+                            <p><FontAwesomeIcon icon={faStar} className='text-yellow-500' />
+                                <span> {data.rating ? data.rating.rate : "99"} average ratings <span className='font-semibold'> {`(${data.rating ? data.rating.count : "99"}) reviews`}</span></span>
                             </p>
-
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <p>Quantity</p>
-                            <div className="mt-2 ">
-                                <button className='border border-black px-2 hover:bg-slate-300' onClick={() => {
-                                    if (count != 1) {
-                                        setCount(count - 1)
+                            <p className="text-lg ">
+                                {data.description}
+                            </p>
+                            <div className="flex gap-4">
+                                <span>Size:</span>
+                                <p className='flex gap-3 items-center '>
+                                    {
+                                        [...Array(3)].map(
+                                            (data, index) => {
+                                                return (
+                                                    <button className='border border-black px-2 hover:bg-slate-300'>
+                                                        {index + 12}
+                                                    </button>
+                                                )
+                                            }
+                                        )
                                     }
-                                }}>-</button>
-                                <button className='border border-black px-4 hover:bg-slate-300' >{count}</button>
-                                <button className='border border-black px-2 hover:bg-slate-300' onClick={() => { setCount(count + 1) }}>+</button>
-                            </div>
+                                </p>
 
-                        </div>
-                        <div className="  flex gap-4 items-center">
-                            <button className='bg-yellow-500 px-5 py-2 text-xl  rounded-full text-white font-semibold'>buy it now</button>
-                            <button className='bg-yellow-500 px-5 py-2 text-xl  rounded-full text-white font-semibold'>add to cart</button>
+                            </div>
+                            <div className="flex items-center gap-4">
+                                <p>Quantity:</p>
+                                <div className="mt-2 ">
+                                    <button className='border border-black px-2 hover:bg-slate-300' onClick={() => {
+                                        if (count != 1) {
+                                            setCount(count - 1)
+                                        }
+                                    }}>-</button>
+                                    <button className='border border-black px-4 hover:bg-slate-300' >{count}</button>
+                                    <button className='border border-black px-2 hover:bg-slate-300' onClick={() => { setCount(count + 1) }}>+</button>
+                                </div>
+
+                            </div>
+                            <div className="  flex gap-4 items-center">
+                                <button className='bg-yellow-500 px-5 py-2 text-xl  rounded-full text-white font-semibold'>buy it now</button>
+                                <button className='bg-yellow-500 px-5 py-2 text-xl  rounded-full text-white font-semibold'>add to cart</button>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="">
+                <h2 className='text-xl font-bold pt-2'>Similar Products</h2>
+                <div className="">
+                    <FetchProducts />
                 </div>
             </div>
         </section>
--- e-commerce-site/app/login/page.js
+++ e-commerce-site/app/login/page.js
@@ -1,7 +1,21 @@
+"use client"
 export default function Page() {
     return (
         <section>
-
+            <div className="h-screen w-screen absolute t-0 l-0 bg-white z-[99] flex justify-center items-center">
+                <div className=" h-[300px]">
+                    <h2 className="text-lg font-bold">Login to your account</h2>
+                    <div className="mt-2">
+                        <form action="" onSubmit={(e) => { e.preventDefault(); alert("Login failed") }}>
+                            <div className="flex flex-col gap-4">
+                                <input className="border border-black px-2 py-[2px] rounded" type="email" placeholder="email" name="" id="" />
+                                <input className="border border-black px-2 py-[2px] rounded" type="password" placeholder="password" name="" id="" />
+                                <input className="border border-black py-[2px] rounded cursor-pointer" type="submit" name="" id="" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
